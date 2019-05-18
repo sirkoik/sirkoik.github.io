bc47c2faf5122cd9fbf8112cc5e2fdaa3a132081
Alexander Koik-Cestone <akoikcestone@gmail.com>
2019-05-17 20:06:12 -0400

    Made title less verbose and reorganized footer.

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,21 @@
 const VERSION = 1.0;
-const LAST_UPDATED = 1558117727089; // timestamp
+const LAST_UPDATED = 1558136678052; // timestamp
 
 var skHome = new function() {
+    
+    // populate the time stamp fiold with a locale-friendly, concisely-formatted date and time.
     this.populateTS = function() {
         var dateUpdated = new Date(LAST_UPDATED);
-        //var options = {};
-        var date = dateUpdated.toDateString(dateUpdated); // try using toLocaleDateString for more options
-        document.getElementById('ts').innerHTML = date; 
+        var options = {
+            month:  'numeric', 
+            day:    'numeric', 
+            year:   'numeric', 
+            hour:   'numeric', 
+            minute: 'numeric'
+        };
+        var date = dateUpdated.toLocaleDateString(dateUpdated, options);
+        date = date.replace(',', '');
+        document.getElementById('edit_timestamp').innerHTML = date; 
     }
 }
 
