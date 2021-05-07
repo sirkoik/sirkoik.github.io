a342de37b644850cfdd94055341280bb2ab63840
Alexander Koik-Cestone <akoikcestone@gmail.com>
2021-05-07 07:23:26 -0400

    Added certs/classes

--- resources/scripts/home.js
+++ resources/scripts/home.js
@@ -1,5 +1,5 @@
-const VERSION = '1.1.0';
-const LAST_UPDATED = 'May 7 2021 6:07 AM';
+const VERSION = '1.1.1';
+const LAST_UPDATED = 'May 7 2021 7:24 AM;
 
 // SkHome.loadEvents
 // activate the panel link event listeners.
