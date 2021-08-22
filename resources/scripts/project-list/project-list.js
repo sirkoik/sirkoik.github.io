f96879bfcf68164fa0b7cfc9d2a34911d7fb39d9
Alexander Koik-Cestone <akoikcestone@gmail.com>
2021-08-22 01:55:00 -0400

    Add Lighthouse skill, implement some Lighthouse-suggested changes

--- resources/scripts/project-list/project-list.js
+++ resources/scripts/project-list/project-list.js
@@ -9,41 +9,46 @@ import { techTypes } from './tech-types.js';
 //    minute: 'numeric'
 //};
 
-
 // renderProjectList: Output the project list to the DOM.
 export const renderProjectList = () => {
-    const container = document.querySelector('.project-container');
+  const container = document.querySelector('.project-container');
 
-    // reverse order of projects to place the newest first
-    projects.reverse();
-    
-    // generate project HTML and add to DOM
-    const projectListHTML = document.createElement('ol');
-    
-    for (const project of projects) {
-        //const projectDate = new Date(project.date).toLocaleDateString(undefined, dateFormat);
-        
-        let technologies = '';
-        if (project.technologies) {
-            technologies = ' ' + project.technologies.map(technology => {
-                const tech = techTypes[technology];
-                return `<img onclick="alert('Technology type: ${tech.name}');" class="tech-image" src="./resources/images/icons/technologies/${tech.image}" alt=${tech.name} title=${tech.name} />`;
-            }).join('');
-        }
+  // reverse order of projects to place the newest first
+  projects.reverse();
 
-        let longDesc = '';
-        if (project.longDescription) {
-            longDesc = '<ol class="skills">';
-            longDesc += project.longDescription.map(item => `<li><span class="list-item-spacer">${item}</span></li>`).join('\n');
-            longDesc += '</ol>';
-        }
+  // generate project HTML and add to DOM
+  const projectListHTML = document.createElement('ol');
 
-        const listItem = document.createElement('li');
-        const itemHTML = `<span class="list-item-spacer"><a href="${project.url}" target="_blank">${project.title}</a> ${project.description}${technologies}${longDesc}</span>`;
-    
-        listItem.innerHTML = itemHTML;
-        projectListHTML.appendChild(listItem);
+  for (const project of projects) {
+    //const projectDate = new Date(project.date).toLocaleDateString(undefined, dateFormat);
+
+    let technologies = '';
+    if (project.technologies) {
+      technologies =
+        ' ' +
+        project.technologies
+          .map((technology) => {
+            const tech = techTypes[technology];
+            return `<img onclick="alert('Technology type: ${tech.name}');" class="tech-image" src="./resources/images/icons/technologies/${tech.image}" alt=${tech.name} title=${tech.name} />`;
+          })
+          .join('');
     }
-    
-    container.appendChild(projectListHTML);
-}
+
+    let longDesc = '';
+    if (project.longDescription) {
+      longDesc = '<ol class="skills">';
+      longDesc += project.longDescription
+        .map((item) => `<li><span class="list-item-spacer">${item}</span></li>`)
+        .join('\n');
+      longDesc += '</ol>';
+    }
+
+    const listItem = document.createElement('li');
+    const itemHTML = `<span class="list-item-spacer"><a href="${project.url}" target="_blank" rel="noopener">${project.title}</a> ${project.description}${technologies}${longDesc}</span>`;
+
+    listItem.innerHTML = itemHTML;
+    projectListHTML.appendChild(listItem);
+  }
+
+  container.appendChild(projectListHTML);
+};
